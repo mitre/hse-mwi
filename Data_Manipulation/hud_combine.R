9f20e4195f6d2c673cd5c8672f1591bcb598c068
Karen Jiang <kjiang@mitre.org>
2021-06-11 14:17:50 -0700

    Fix calculation for black housing stress by adjusting denominator

--- Data_Manipulation/hud_combine.R
+++ Data_Manipulation/hud_combine.R
@@ -3,35 +3,48 @@
 # Originated on: 6/11/21
 
 library(tidyverse)
-# load data and combine ----
 
+# load data and combine ----
 data_folder <- file.path(
   gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
   "Health and Social Equity - SJP - BHN Score Creation",
   "Data", "Raw", "HUD")
 
 
-df <- read.csv(paste0(data_folder, "/Table1.csv"))
+# Read in Table
+df <- read.csv(file.path(data_folder, "Table1.csv"))
+
+# Read in Data Dictionary
+dictionary <- readxl::read_xlsx(file.path(data_folder, 
+                                          "CHAS data dictionary 13-17.xlsx"),
+                                sheet = "Table 1")
+
+
+# Using Data dictionary to identify the column numbers for numerator and denom
+black_num <- dictionary %>%
+  filter(`Race/ethnicity` == "Black or African-American alone, non-Hispanic",
+         `1 of 4 housing problems` == "has 1 or more of the 4 housing unit problems (lacks kitchen or plumbing, more than 1 person per room, or cost burden greater than 30%)")
+black_denom <- dictionary %>%
+  filter(`Race/ethnicity` == "Black or African-American alone, non-Hispanic")
+
+black_num_col_nums <- unlist(which(colnames(df) %in% black_num$`Column Name`))
+black_denom_col_nums <- unlist(which(colnames(df) %in% black_denom$`Column Name`))
+
+
 
 housing_stress <- df %>% 
   # Converting existing geoid into 11-digit ID
   separate(geoid, into = c("country code", "geoid"), sep = "US") %>%
-  
-  # T1_est3 :  Owner Occupied - has 1 or more of the 4 housing unit problems 
-  # (lacks kitchen or plumbing, more than 1 person per room, or cost burden greater than 30%)
-  # T1_est127: Renter Occupied - has 1 or more of the 4 housing unit problems 
-  # (lacks kitchen or plumbing, more than 1 person per room, or cost burden greater than 30%)
-  # T1_est2: All Owner Occupied
-  # T1_est126: All Renter Occupied
+         # Calculate housing stress for general pop
+         # Defined as all pop with housing stress / all renters & owners
   mutate(housing_stress_pop = (T1_est3 + T1_est127)/(T1_est2 + T1_est126) * 100,
          
-         housing_stress_black = (
-           # Owner Occupied - has 1 or more of 4 housing unit problems - Black or African American - All household incomes
-           T1_est6 + T1_est14 + T1_est22 + T1_est30 + T1_est38 +
-           # Renter Occupied - has 1 or more of 4 housing unit problems - Black or African American - All household incomes
-           T1_est130 + T1_est138 + T1_est146 + T1_est154 + T1_est162) /
-          
-           (T1_est2 + T1_est126) * 100) %>%
+         # Calculate housing stress for black pop
+         # Defined as black pop with housing stress / black renters & owners
+         black_num = select(., black_num_col_nums) %>% rowSums(na.rm = T),
+         black_denom = select(., black_denom_col_nums) %>% rowSums(na.rm = T),
+         housing_stress_black = black_num/black_denom * 100) %>%
+  
   select(geoid, housing_stress_pop, housing_stress_black)
   
 
