07f96ee23acebbabb1c7936c24255a604e708f08
Emily Pantalone <epantalone@mitre.org>
2021-06-16 17:52:14 -0400

    - add new column for raw ratio for violent crime - scale existing ratio to per 10000 population - explore outliers that could be clipped

--- Data_Manipulation/violent_crime_data.R
+++ Data_Manipulation/violent_crime_data.R
@@ -13,6 +13,7 @@ data_folder <- file.path(
   "Data", "Raw")
 
 vc_data <- read_csv(file.path(data_folder,"analytic_data2020_0.csv"))
+# og_vc_data <- read_csv(file.path(data_folder,"analytic_data2020_0.csv"))
 
 #explore data dimensions
 dim(vc_data)
@@ -24,8 +25,6 @@ vc_data <- vc_data[-c(1,2),]
 #delete unneeded columns
 vc_data <- vc_data[c(1,2,3,4,5,233,234,235)]
 
-#remove state aggregate rows
-vc_data <- vc_data[vc_data$'County FIPS Code' != '000', ] 
 
 #rename columns
 names(vc_data)[names(vc_data) == "County FIPS Code"] <- "county_FIPS"
@@ -36,6 +35,34 @@ names(vc_data)[names(vc_data) == "Name"] <- "county"
 names(vc_data)[names(vc_data) == "Violent crime numerator"] <- "violent_crime_num"
 names(vc_data)[names(vc_data) == "Violent crime denominator"] <- "violent_crime_denom"
 names(vc_data)[names(vc_data) == "Violent crime raw value"] <- "violent_crime_value"
+
+
+#remove state aggregate rows
+vc_data <- vc_data[vc_data$county_FIPS != '000', ] 
+
+#change violent crime numbers to integers (currently is a character)
+vc_data$violent_crime_num <- as.numeric(vc_data$violent_crime_num)
+vc_data$violent_crime_denom <- as.numeric(vc_data$violent_crime_denom)
+vc_data$violent_crime_value <- as.numeric(vc_data$violent_crime_value)
+
+#create column with raw integer of violent crimes per population (num/denom), 
+# not per 100,000 population as violent_crime_value currently indicates
+vc_data$violent_crime_ratio <- vc_data$violent_crime_num/vc_data$violent_crime_denom
+
+#scale violent_crime_value to be per 10,000 population
+vc_data$violent_crime_value <- vc_data$violent_crime_value/10
+
+#explore outliers that may need to be clipped and count NAs
+summary(vc_data$violent_crime_value)
+#library(ggplot2)
+#boxplot(vc_data$violent_crime_value)
+#sum(vc_data$violent_crime_value > 100, na.rm = TRUE)
+#nrow(vc_data)
+
+# There are 23 values that are > 100 that could be clipped, out of 3142 values (0.7%)
+# There are 191 NAs out of 3142 possible values (6% missing)
+
+
 
 # write out ----
 
