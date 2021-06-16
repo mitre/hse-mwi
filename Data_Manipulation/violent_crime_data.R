11ea182c518761c6144a22269214585c00d642a2
Emily Pantalone <epantalone@mitre.org>
2021-06-16 11:15:43 -0400

    rename columns, delete state aggregate rows

--- Data_Manipulation/violent_crime_data.R
+++ Data_Manipulation/violent_crime_data.R
@@ -24,12 +24,20 @@ unique(vc_data$`State Abbreviation`)
 vc_data <- vc_data[-c(1,2),]
 
 #delete unneeded columns
-vc_data <- vc_data[c(1,2,3,4,5,6,7,233,234,235)]
+vc_data <- vc_data[c(1,2,3,4,5,7,233,234,235)]
+
+#remove state aggregate rows
+vc_data <- vc_data[vc_data$'County FIPS Code' != '000', ] 
 
 #rename columns
-
-
-
+names(vc_data)[names(vc_data) == "County FIPS Code"] <- "county_FIPS"
+names(vc_data)[names(vc_data) == "5-digit FIPS Code"] <- "full_FIPS"
+names(vc_data)[names(vc_data) == "State FIPS Code"] <- "state_FIPS"
+names(vc_data)[names(vc_data) == "State Abbreviation"] <- "state"
+names(vc_data)[names(vc_data) == "Name"] <- "county"
+names(vc_data)[names(vc_data) == "Violent crime numerator"] <- "violent_crime_num"
+names(vc_data)[names(vc_data) == "Violent crime denominator"] <- "violent_crime_denom"
+names(vc_data)[names(vc_data) == "Violent crime raw value"] <- "violent_crime_value"
 
 # write out ----
 
