2a71ba77465262ee78559cd0838630ea09da08e7
Emily Pantalone <epantalone@mitre.org>
2021-06-22 11:31:04 -0400

    integrate Hannah's notes:  - colnames function  - rename columns  - remove data/outlier exploration functions

--- Data_Manipulation/violent_crime_data.R
+++ Data_Manipulation/violent_crime_data.R
@@ -12,12 +12,8 @@ data_folder <- file.path(
   "Health and Social Equity - SJP - BHN Score Creation",
   "Data", "Raw")
 
-vc_data <- read_csv(file.path(data_folder,"analytic_data2020_0.csv"))
-# og_vc_data <- read_csv(file.path(data_folder,"analytic_data2020_0.csv"))
+vc_data<- read_csv(file.path(data_folder,"analytic_data2020_0.csv"))
 
-#explore data dimensions
-dim(vc_data)
-colnames(vc_data)
 
 #delete first two rows (they are aggregate, not county-specific)
 vc_data <- vc_data[-c(1,2),]
@@ -27,40 +23,20 @@ vc_data <- vc_data[c(1,2,3,4,5,233,234,235)]
 
 
 #rename columns
-names(vc_data)[names(vc_data) == "County FIPS Code"] <- "county_FIPS"
-names(vc_data)[names(vc_data) == "5-digit FIPS Code"] <- "full_FIPS"
-names(vc_data)[names(vc_data) == "State FIPS Code"] <- "state_FIPS"
-names(vc_data)[names(vc_data) == "State Abbreviation"] <- "state"
-names(vc_data)[names(vc_data) == "Name"] <- "county"
-names(vc_data)[names(vc_data) == "Violent crime numerator"] <- "violent_crime_num"
-names(vc_data)[names(vc_data) == "Violent crime denominator"] <- "violent_crime_denom"
-names(vc_data)[names(vc_data) == "Violent crime raw value"] <- "violent_crime_value"
-
+colnames(vc_data)<- c("stateFIPS","countyFIPS","fullFIPS","state","county",
+                      "violentcrime_incidentsper100000_pop","violentcrime_incidents_pop", 
+                      "violentcrime_totalpopulation_pop")
 
 #remove state aggregate rows
-vc_data <- vc_data[vc_data$county_FIPS != '000', ] 
+vc_data <- vc_data[vc_data$countyFIPS != '000', ] 
 
 #change violent crime values to numbers (currently is a character)
-vc_data$violent_crime_num <- as.numeric(vc_data$violent_crime_num)
-vc_data$violent_crime_denom <- as.numeric(vc_data$violent_crime_denom)
-vc_data$violent_crime_value <- as.numeric(vc_data$violent_crime_value)
+vc_data$violentcrime_incidents_pop <- as.numeric(vc_data$violentcrime_incidents_pop)
+vc_data$violentcrime_totalpopulation_pop <- as.numeric(vc_data$violentcrime_totalpopulation_pop)
+vc_data$violentcrime_incidentsper100000_pop <- as.numeric(vc_data$violentcrime_incidentsper100000_pop)
 
 #create column with raw ratio of violent crimes per population (num/denom), 
-# not per 100,000 population as violent_crime_value currently indicates
-vc_data$violent_crime_ratio <- vc_data$violent_crime_num/vc_data$violent_crime_denom
-
-#scale violent_crime_value to be per 10,000 population
-vc_data$violent_crime_value <- vc_data$violent_crime_value/10
-
-#explore outliers that may need to be clipped and count NAs
-summary(vc_data$violent_crime_value)
-#library(ggplot2)
-#boxplot(vc_data$violent_crime_value)
-#sum(vc_data$violent_crime_value > 100, na.rm = TRUE)
-#nrow(vc_data)
-
-# There are 23 values that are > 100 that could be clipped, out of 3142 values (0.7%)
-# There are 191 NAs out of 3142 possible values (6% missing)
+vc_data$violentcrime_incidenttopopulationratio_pop <- vc_data$violentcrime_incidents_pop/vc_data$violentcrime_totalpopulation_pop
 
 
 # write out ----
