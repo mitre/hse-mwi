ea45fc102ab8e4aea761efbf4831ab1a1152ab99
Karen Jiang <kjiang@mitre.org>
2021-07-21 14:45:20 -0700

    Transfer initial MPV work from data analysis to combine file

--- Data_Manipulation/mpv_combine.R
+++ Data_Manipulation/mpv_combine.R
@@ -1,9 +1,11 @@
-# Combining HUD data
+# Combining Mapping Police Violence Data
 # By Karen Jiang
 # Originated on: 6/11/21
 
 library(tidyverse)
 library(tidycensus)
+library(lubridate)
+
 
 # Read in datasets ----
 
@@ -13,13 +15,35 @@ data_folder <- file.path(
   "Health and Social Equity - SJP - BHN Score Creation",
   "Data")
 
-# Read in Mapping Police Violence Data
+# Read & Clean Mapping Police Violence Data -----
     # Data last pulled June 25th, 2021
     # https://mappingpoliceviolence.org/
 mpv <- readxl::read_xlsx(file.path(data_folder,"Raw", "MPVDatasetDownload.xlsx"))
 
+# Reformatting dates
+mpv$`Date of Incident (month/day/year)` <- as.Date(mpv$`Date of Incident (month/day/year)`)
+mpv$year <- year(mpv$`Date of Incident (month/day/year)`)
+mpv$month <- month(mpv$`Date of Incident (month/day/year)`)
 
-# Read in crosswalk files
+# Standardizing ZIP Codes 
+mpv$Zipcode <- str_pad(as.character(mpv$Zipcode), width = 5, side = "left", pad = "0")
+
+# Removing any rows with ZIP Codes that are longer than 5 digits
+mpv$ziplength = nchar(mpv$Zipcode)
+mpv <- mpv[mpv$ziplength == 5,]
+mpv$ziplength <- NULL
+
+# Fixed labeling in Race Category 
+mpv <- mpv %>%
+  mutate(`Victim's race` = case_when(`Victim's race` == "white" ~ "White",
+                                     `Victim's race` == "Unknown Race" ~ "Unknown race",
+                                     TRUE ~ as.character(`Victim's race`))) %>%
+  # Select relevant vars
+  select(`Victim's race`, year, Zipcode, State, County) %>%
+  # Remove rows with missing data
+  na.omit() 
+
+# Read in crosswalk files -----
 zip_to_zcta <- read.csv(file.path(data_folder, "Resources", "Zip_to_zcta_crosswalk_2020.csv"))
 zip_to_zcta$ZIP_CODE = str_pad(zip_to_zcta$ZIP_CODE,
                                width = 5,
@@ -37,18 +61,16 @@ zcta_to_county$GEOID <- str_pad(zcta_to_county$GEOID,
                                 side = "left",
                                 pad = "0")
 
-# Pull ACS denominators for total pop and black pop
+# Pull ACS denominators -----
 
-# Getting ACS population data from 2013-2019
-if (!file.exists("data/ACS ZCTA Population 2013-2019.Rds")) {
-  
+# Getting ACS population data from 2013-2019 for total and black pop
   years <- 2013:2019
   
   get_pop_data <- function(year){
     get_acs(geography = "zcta", 
             survey = "acs5",
             variables = c(total_pop = "B01001_001E",
-                          black_pop = "B01001"),
+                          black_pop = "B01001B_001E"),
             year = year)
   }
   
@@ -58,34 +80,19 @@ if (!file.exists("data/ACS ZCTA Population 2013-2019.Rds")) {
     separate(NAME, into = c("label", "ZCTA")) %>%
     select(-variable, -moe, -label, -GEOID) %>%
     mutate(year = as.numeric(year) + 2012) 
+
   
-  saveRDS(pop_data_years,file = "data/ACS ZCTA Population 2013-2019.Rds")
-  
-} else {
-  
-  load("data/ACS ZCTA Population 2013-2019.Rds")
-}
 
 
 # Clean Up MPV Data -------
-
-# Reformatting dates
-mpv$`Date of Incident (month/day/year)` <- as.Date(mpv$`Date of Incident (month/day/year)`)
-mpv$year <- year(mpv$`Date of Incident (month/day/year)`)
-mpv$month <- month(mpv$`Date of Incident (month/day/year)`)
-
-# Standardizing ZIP Codes 
-mpv$Zipcode <- str_pad(as.character(mpv$Zipcode), width = 5, side = "left", pad = "0")
-
-# Fixed labeling in Race categroy
-mpv <- mpv %>%
-  mutate(`Victim's race` = case_when(`Victim's race` == "white" ~ "White",
-                                     `Victim's race` == "Unknown Race" ~ "Unknown race",
-                                     TRUE ~ as.character(`Victim's race`)))
+  ## Improving the precision of estimates of the frequency of rare events (2005)
+  ## Method 4: Inclusion of aggregated data collected at larger spatial or temporal scales
+  ## https://lib.dr.iastate.edu/cgi/viewcontent.cgi?article=1060&context=stat_las_pubs
 
 
 
-# write out ----
+
+# Write out ----
 
 data_folder <- file.path(
   gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
