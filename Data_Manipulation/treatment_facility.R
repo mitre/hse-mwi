80d3467ef3b29f8316abe4716ba1781a4ab4572d
Mitch Strahlman <mstrahlman@mitre.org>
2021-08-04 12:29:24 -0500

    Move functions and results to appropriate sections

--- Data_Manipulation/treatment_facility.R
+++ Data_Manipulation/treatment_facility.R
@@ -43,6 +43,112 @@ poly <- get_decennial(geography = "tract", state = states,
                       variables = "P012001", year = 2010, geometry = T) %>%
   mutate(county = substr(GEOID, 1, 5))
 
+# Step 1 of Floating Catchment Area (FCA) Methodology----
+# Build a Function to Calculate R value
+# For each treatment center, search all CTs within the CT distance threshold
+# and sum their populations to compute the treatment-to-popuation ratio (R)
+
+# Build a function for Step 1 of the 2 Step FCA Method
+# Inputs
+#   data: dataset of facilities with specified point data
+#   geo: dataset of geographical points
+# Output  
+#   Returns R value for each facility within the dataset of facilities
+
+step_1_fca <- function (data, geo) {
+  # Build a for loop while subseting by proximity - only look at CT centroids
+  # within +/- 1 lat/long of facility
+  # Create for loop
+  geo <- geo %>%
+    add_column(d = as.numeric(NA))
+  # Keep the coordinates 
+  geo_coord <- st_coordinates(geo)
+  fac_coord <- st_coordinates(data)
+  start <- Sys.time()
+  for (i in 1:1000) {
+    # print every 50
+    if (i %% 50 == 0){
+      print(paste0("[", Sys.time(), "] Currently on iteration: ", i))
+    }
+    
+    # Compute distances between facility and CT centroids within +/-1 proximity
+    # for first 1000 MH treatment centers
+    # Keep the logical 
+    prox_log <- geo_coord[,2] < fac_coord[i,2] + 1 &
+      geo_coord[,2] > fac_coord[i,2] - 1 &
+      geo_coord[,1] < fac_coord[i,1] + 1 &
+      geo_coord[,1] > fac_coord[i,1] - 1
+    
+    geo$d[prox_log] <- 
+      raster::pointDistance(
+        data[i,], 
+        geo[prox_log, ], lonlat = T) * .0006213712
+    # Calculate the r value
+    # Step 1 of 2 Step Floating Catchment Area (FCA) Methodology
+    data$R[i] <- 1/sum(filter(geo, d < data$Distance[i])$POPULATION)
+    
+    # Preallocate and reuse distance values
+    geo$d[prox_log] <- NA
+  }
+  end <- Sys.time()
+  print(end - start)
+  data
+}
+
+# Step 2 of FCA Methodology----
+
+# Build a function to Calculate A value
+# Search all treatment centers that are within the distance threshold from 
+# each CT and sum their r values per each CT 
+
+# Inputs
+#   data: dataset of faciltiies with specified r values
+#   geo: dataset of geographical points
+#   facil: the type of facility (MH, SA, etc) in character format
+# Output  
+#   Returns A value for each geography within the dataset of geographical points
+
+step_2_fca <- function (data, geo, facil) {
+  # Build a for loop while subseting by proximity - only look at facilities
+  # within +/- 1 lat/long of CT Centroids
+  data <- data %>%
+    add_column(d = as.numeric(NA))
+  # Keep coordinates
+  geo_coord <- st_coordinates(geo)
+  fac_coord <- st_coordinates(data)
+  start <- Sys.time()
+  for (i in 1:1000) {
+    # print every 50
+    if (i %% 50 == 0){
+      print(paste0("[", Sys.time(), "] Currently on iteration: ", i))
+    }
+    # Compute distances between facility and CT centroids within +/-1 proximity
+    # for first 1000 CT centroids
+    # Keep the logical
+    prox_log <- geo_coord[i,2] < fac_coord[,2] + 1 &
+      geo_coord[i,2] > fac_coord[,2] - 1 &
+      geo_coord[i,1] < fac_coord[,1] + 1 &
+      geo_coord[i,1] > fac_coord[,1] - 1
+    
+    data$d[prox_log] <- 
+      raster::pointDistance(
+        data[prox_log,], 
+        geo[i, ], lonlat = T) * .0006213712
+    # Calculate the A value
+    # Sum all r values within the distance threshold for each CT
+    geo$A[i] <- sum(filter(data, d < geo[["Distance"]][i])$r)
+    # Preallocate and reuse distance values
+    data$d[prox_log] <- NA
+  }
+  end <- Sys.time()
+  print(end - start)
+  # Set A variable name based on dataset being used in function
+  colnames(geo)[which(names(geo) == "A")] <- paste0("A",
+                                                    "_",
+                                                    facil)
+  geo
+}
+
 # Wrangle and Merge Data for Usability----
 # Create Census Tract Variable for Thresholds 
 thresh <- thresh %>%
@@ -172,58 +278,7 @@ mh_fac <- add_column(mh_fac, weights = 1, .before = "psy")
 # Add 1 to all weights in SH dataset
 sa_fac <- add_column(sa_fac, weights = 1, .before = "dt")
 
-# Step 1 of Floating Catchment Area (FCA) Methodology----
-# Build a Function to Calculate R value
-# For each treatment center, search all CTs within the CT distance threshold
-# and sum their populations to compute the treatment-to-popuation ratio (R)
-
-# Build a function for Step 1 of the 2 Step FCA Method
-# Inputs
-#   data: dataset of facilities with specified point data
-#   geo: dataset of geographical points
-# Output  
-#   Returns R value for each facility within the dataset of facilities
-
-step_1_fca <- function (data, geo) {
-  # Build a for loop while subseting by proximity - only look at CT centroids
-  # within +/- 1 lat/long of facility
-  # Create for loop
-  geo <- geo %>%
-    add_column(d = as.numeric(NA))
-  # Keep the coordinates 
-  geo_coord <- st_coordinates(geo)
-  fac_coord <- st_coordinates(data)
-  start <- Sys.time()
-  for (i in 1:1000) {
-    # print every 50
-    if (i %% 50 == 0){
-      print(paste0("[", Sys.time(), "] Currently on iteration: ", i))
-    }
-    
-    # Compute distances between facility and CT centroids within +/-1 proximity
-    # for first 1000 MH treatment centers
-    # Keep the logical 
-    prox_log <- geo_coord[,2] < fac_coord[i,2] + 1 &
-      geo_coord[,2] > fac_coord[i,2] - 1 &
-      geo_coord[,1] < fac_coord[i,1] + 1 &
-      geo_coord[,1] > fac_coord[i,1] - 1
-    
-    geo$d[prox_log] <- 
-      raster::pointDistance(
-        data[i,], 
-        geo[prox_log, ], lonlat = T) * .0006213712
-    # Calculate the r value
-    # Step 1 of 2 Step Floating Catchment Area (FCA) Methodology
-    data$R[i] <- 1/sum(filter(geo, d < data$Distance[i])$POPULATION)
-    
-    # Preallocate and reuse distance values
-    geo$d[prox_log] <- NA
-  }
-  end <- Sys.time()
-  print(end - start)
-  data
-}
-
+# Results----
 # Create R values for MH dataset
 mh_fac <- step_1_fca(mh_fac, ct) %>%
   relocate(R, .after = POPULATION)
@@ -232,64 +287,8 @@ mh_fac <- step_1_fca(mh_fac, ct) %>%
 sa_fac <- step_1_fca(sa_fac, ct) %>%
   relocate(R, .after = POPULATION) 
 
-# Step 2 of FCA Methodology----
-
-# Build a function to Calculate A value
-# Search all treatment centers that are within the distance threshold from 
-# each CT and sum their r values per each CT 
-
-# Inputs
-#   data: dataset of faciltiies with specified r values
-#   geo: dataset of geographical points
-#   facil: the type of facility (MH, SA, etc) in character format
-# Output  
-#   Returns A value for each geography within the dataset of geographical points
-
-step_2_fca <- function (data, geo, facil) {
-  # Build a for loop while subseting by proximity - only look at facilities
-  # within +/- 1 lat/long of CT Centroids
-  data <- data %>%
-    add_column(d = as.numeric(NA))
-  # Keep coordinates
-  geo_coord <- st_coordinates(geo)
-  fac_coord <- st_coordinates(data)
-  start <- Sys.time()
-  for (i in 1:1000) {
-    # print every 50
-    if (i %% 50 == 0){
-      print(paste0("[", Sys.time(), "] Currently on iteration: ", i))
-    }
-    # Compute distances between facility and CT centroids within +/-1 proximity
-    # for first 1000 CT centroids
-    # Keep the logical
-    prox_log <- geo_coord[i,2] < fac_coord[,2] + 1 &
-      geo_coord[i,2] > fac_coord[,2] - 1 &
-      geo_coord[i,1] < fac_coord[,1] + 1 &
-      geo_coord[i,1] > fac_coord[,1] - 1
-    
-    data$d[prox_log] <- 
-      raster::pointDistance(
-        data[prox_log,], 
-        geo[i, ], lonlat = T) * .0006213712
-    # Calculate the A value
-    # Sum all r values within the distance threshold for each CT
-    geo$A[i] <- sum(filter(data, d < geo[["Distance"]][i])$r)
-    # Preallocate and reuse distance values
-    data$d[prox_log] <- NA
-  }
-  end <- Sys.time()
-  print(end - start)
-  # Set A variable name based on dataset being used in function
-  colnames(geo)[which(names(geo) == "A")] <- paste0("A",
-                                                   "_",
-                                                   facil)
-  geo
-}
-
 # Create A values using MH dataset
 ct <- step_2(mh_fac, ct, "mh")
 
 # Create A values using SA dataset
 ct <- step_2(sa_fac, ct, "sa")
-
-# Results----
