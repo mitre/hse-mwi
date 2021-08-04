edccd144b0f881088724b9f4fda5063f37a8dfec
Dr. Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2021-08-04 09:55:52 -0400

    Create financial accessibility score

--- Data_Manipulation/financial_access.R
+++ Data_Manipulation/financial_access.R
@@ -5,6 +5,7 @@
 # load data and libraries ----
 
 library(censusapi)
+library(stringr)
 
 resource_folder <-file.path(
   gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
@@ -24,19 +25,23 @@ data_folder <- file.path(
   "Data", "Raw", "FDIC_NCUA")
 
 # federal banking institutions
-fed_banks <- read.csv(file.path(data_folder, "FDIC_Institutions_7_29_2021.csv"))
-# federal deposit
-# ADDRESS street address
-# CITY city
-# COUNTY county
-# STNAME state name
-# STNUM state number
-# ZIP zip code
+fed_banks <- read.csv(
+  file.path(data_folder, "FDIC_Institutions_7_29_2021.csv"),
+  colClasses = c("ZIP" = "character")
+)
 
 # credit unions
 cu_banks <- read.csv(file.path(
   data_folder, 
-  "NCUA_Credit_Union_Branch_Information_03_2021.csv"))
+  "NCUA_Credit_Union_Branch_Information_03_2021.csv"),
+  colClasses = c("PhysicalAddressPostalCode" = "character"))
+# clean up zip codes
+cu_banks$PhysicalAddressPostalCode <- 
+  gsub("-.*", "", cu_banks$PhysicalAddressPostalCode)
+# pad with leading 0s
+cu_banks$PhysicalAddressPostalCode <- 
+  str_pad(cu_banks$PhysicalAddressPostalCode, 5, pad = "0")
+
 # naics_codes
 naics_codes <- read.csv(file.path(
   data_folder, 
@@ -74,3 +79,30 @@ for (nc in unique(naics_codes$NAICS_2017)){
       zbp_service$ESTAB
   }
 }
+
+# create overall financial accessibility score ----
+
+# create the financial accessibility overall df
+# already contains alternative services
+fin_access <- alt_service
+
+# preallocate mainstream services
+fin_access$Num_Mainstream_Services <- 0
+# count and add FDIC banks
+fdic_count <- as.data.frame(table(as.character(fed_banks$ZIP)), 
+                            stringsAsFactors = F)
+fin_access[fdic_count$Var1, "Num_Mainstream_Services"] <-
+  fin_access[fdic_count$Var1, "Num_Mainstream_Services"] +
+  fdic_count$Freq
+# count and add credit unions
+cu_count <- as.data.frame(
+  table(as.character(cu_banks$PhysicalAddressPostalCode)), 
+  stringsAsFactors = F
+)
+fin_access[cu_count$Var1, "Num_Mainstream_Services"] <-
+  fin_access[cu_count$Var1, "Num_Mainstream_Services"] +
+  cu_count$Freq
+
+# create financial accessibility score
+fin_access$Financial_Accessibility <- 
+  fin_access$Num_Alt_Services/fin_access$Num_Mainstream_Services
