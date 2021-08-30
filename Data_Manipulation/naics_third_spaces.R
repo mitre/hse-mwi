ef5cdbde5f8d2ec7815ec45afffc63f222a43b62
Mitch Strahlman <mstrahlman@mitre.org>
2021-08-30 11:32:11 -0500

    load zip codes + input Hannah's read zip function

--- Data_Manipulation/naics_third_spaces.R
+++ Data_Manipulation/naics_third_spaces.R
@@ -6,6 +6,31 @@
 library(censusapi)
 library(tidyverse)
 
+# Supporting Function ----
+# Read in and clean zip code according to common mistakes
+# inputs:
+#   fn: file name (including path)
+#   zip_cn: zip code column name
+# outputs: read and cleaned data frame
+read_zips <- function(fn, zip_cn){
+  # read in specified file
+  zip_df <- read.csv(fn,
+                     colClasses = setNames("character", zip_cn))
+  
+  # remove all dashes
+  zip_df[, zip_cn] <- gsub("-", "", zip_df[,zip_cn])
+  
+  # pad input zips with leading 0s
+  zip_df[, zip_cn] <- str_pad(zip_df[, zip_cn], 5, pad = "0")
+  
+  # get rid of weirdly long ones
+  zip_df[, zip_cn] <- substr(zip_df[, zip_cn], 1, 5)
+  
+  return(zip_df)
+}
+
+
+# Load data----
 # Read in NAICS codes
 data_folder <- file.path(
   gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
@@ -16,3 +41,18 @@ naics_codes <-
   read.csv(file.path(data_folder, "NAICS_Third_Places_aggregation.csv")) %>%
   rename("Code" = "X2017.NAICS.Code",
          "Business_Type" = "X2017.NAICS.Title")
+
+# Read in all zip codes
+resource_folder <- file.path(
+  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
+  "Health and Social Equity - SJP - BHN Score Creation",
+  "Data", "Resources")
+
+# Load all zip codes
+zip <- read_zips(
+  file.path(resource_folder, "Zip_to_zcta_crosswalk_2020.csv"),
+  "ZIP_CODE")
+
+# Filter out Territories
+territories <- c("AS", "FM", "GU", "MH", "MP", "PW", "PR", "VI")
+zip <- zip[!zip$STATE %in% territories, ]
