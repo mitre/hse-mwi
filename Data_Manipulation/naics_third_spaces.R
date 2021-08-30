61b9846012fdf870d5db377a48a3b8bc43431b51
Mitch Strahlman <mstrahlman@mitre.org>
2021-08-30 10:16:03 -0500

    read in naics codes

--- Data_Manipulation/naics_third_spaces.R
+++ Data_Manipulation/naics_third_spaces.R
@@ -3,3 +3,10 @@ library(censusapi)
 library(tidyverse)
 
 # Read in NAICS codes
+data_folder <- file.path(
+  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
+  "Health and Social Equity - SJP - BHN Score Creation",
+  "Data", "Raw", "Third_Spaces")
+
+naics_codes <- 
+  read.csv(file.path(data_folder, "NAICS_Third_Places_aggregation.csv"))
