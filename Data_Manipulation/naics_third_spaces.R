51cb98983855d032409457ba6b7b57b1a70b952e
Mitch Strahlman <mstrahlman@mitre.org>
2021-09-24 08:37:45 -0500

    Comment concerning infinite values

--- Data_Manipulation/naics_third_spaces.R
+++ Data_Manipulation/naics_third_spaces.R
@@ -97,6 +97,9 @@ zip <- zip %>%
   select(-c(2:6, "population")) %>%
   relocate(thirdspaces_pop, .after = ZIP_CODE) %>%
   mutate(thirdspaces_pop = replace(thirdspaces_pop, is.nan(thirdspaces_pop), 0))
+
+# Some zipcodes have no population and third spaces, leading to an Inf value
+# 
   
 # Export Data Frame----
 
