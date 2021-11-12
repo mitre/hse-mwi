68dd3818455e06bbe1c010890ff4413459e6d9c5
Dr. Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2021-11-12 12:45:17 -0500

    Move zip-reading function to a new file (for future)

--- Data_Manipulation/financial_access.R
+++ Data_Manipulation/financial_access.R
@@ -9,27 +9,7 @@ library(stringr)
 
 # supporting functions ----
 
-# function to read in and clean zip code according to common mistakes
-# inputs:
-#   fn: file name (including path)
-#   zip_cn: zip code column name
-# outputs: read and cleaned data frame
-read_zips <- function(fn, zip_cn){
-  # read in specified file
-  zip_df <- read.csv(fn,
-                     colClasses = setNames("character", zip_cn))
-  
-  # remove all dashes
-  zip_df[, zip_cn] <- gsub("-", "", zip_df[,zip_cn])
-  
-  # pad input zips with leading 0s
-  zip_df[, zip_cn] <- str_pad(zip_df[, zip_cn], 5, pad = "0")
-  
-  # get rid of weirdly long ones
-  zip_df[, zip_cn] <- substr(zip_df[, zip_cn], 1, 5)
-  
-  return(zip_df)
-}
+source(file.path("Processing_Pipeline", "read_geo_files.R"))
 
 # load supporting data ----
 
