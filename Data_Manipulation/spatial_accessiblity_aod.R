150186a1092c6af91712afe73ed6797727113f0b
Mitch Strahlman <mstrahlman@mitre.org>
2021-12-29 11:50:04 -0600

    insert resource folders

--- Data_Manipulation/spatial_accessiblity_aod.R
+++ Data_Manipulation/spatial_accessiblity_aod.R
@@ -4,3 +4,16 @@
 library(dplyr)
 library(tidycensus)
 library(censusapi)
+
+source("Processing_Pipeline/crosswalk_func.R")
+
+# Set Data & Resource Folders
+data_folder <- file.path(
+  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
+  "Health and Social Equity - SJP - BHN Score Creation",
+  "Data", "Raw")
+
+resource_folder <-file.path(
+  gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
+  "Health and Social Equity - SJP - BHN Score Creation",
+  "Data", "Resources")
