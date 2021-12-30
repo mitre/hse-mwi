95003d855285f15b0bc3626a487d9b6b070ad3fd
Mitch Strahlman <mstrahlman@mitre.org>
2021-12-30 12:10:52 -0600

    insert state grocery laws data

--- Data_Manipulation/spatial_accessiblity_aod.R
+++ Data_Manipulation/spatial_accessiblity_aod.R
@@ -4,6 +4,7 @@
 library(dplyr)
 library(tidycensus)
 library(censusapi)
+library(readxl)
 
 source("Processing_Pipeline/crosswalk_func.R")
 
@@ -17,3 +18,10 @@ resource_folder <-file.path(
   gsub("\\\\","/", gsub("OneDrive - ","", Sys.getenv("OneDrive"))), 
   "Health and Social Equity - SJP - BHN Score Creation",
   "Data", "Resources")
+
+# Spreadsheet with state by state Grocery Laws
+grocery_states <- read_xlsx(file.path(resource_folder, "BWL_Grocery_Laws.xlsx"),
+                            skip = 1)
+# Create column with indicator if state allows ANY type of alc sales
+grocery_states[,3:5] <- ifelse(grocery_states[,3:5] == "Y", TRUE, FALSE)
+grocery_states$any_alc_sales <- ifelse(rowSums(grocery_states[,3:5] , na.rm = T) > 0, TRUE, FALSE)
