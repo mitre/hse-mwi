4d292cf0278ffeaaf4049a497a19452695ff5942
Dr. Hannah De los Santos <HDELOSSANTOS@MITRE.ORG>
2022-03-10 15:45:36 -0500

    Update image size to update on resize

--- app.R
+++ app.R
@@ -742,7 +742,7 @@ ui <- fluidPage(
             bsCollapsePanel(
               "About the Mental Wellness Index",
               HTML("<center>"),
-              img(src = file.path("media", "MWI Framework (Transparent Background).png"), align = "center", height = "270px"),
+              img(src = file.path("media", "MWI Framework (Transparent Background).png"), align = "center", width = "90%"),
               HTML("</center>"),
               HTML("<font size = '2'>"),
               HTML(paste0("The Mental Wellness Index is the weighted sum of 28 measure values, which quantify facilitators and barriers to mental wellness. For more information about the Mental Wellness Index, please see the 'About' page.<p></p>"
